f27a6c85361fa1d08d9b14839847e99f69383e4a
structuresound <leif@structuresound.com>
2016-02-22 10:02:05 -0800

    server

--- server/client/app.jsx
+++ server/client/app.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-
+import Header from './header/header.jsx';
 // define and export our Layout component
 export const Layout = ({content}) => (
-    <div>
-        <h1>dbMake</h1>
-        <hr />
-        <div>{content}</div>
-    </div>
+  <div>
+    <Header />
+    <div>{content}</div>
+    <footer></footer>
+  </div>
 );
 
 // define and export our Welcome component
 export const Welcome = ({name}) => (
-    <div>
-        Hello, {name}.
+  <div>
+    <div class="container">
+      <section>
+        <aside>
+          Hello,{name}.
+        </aside>
+        <article>
+          This is the deets
+        </article>
+      </section>
     </div>
+  </div>
 );
